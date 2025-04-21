28d238e3c5da162ff96b28492392de06d8ca71c8
Mario Roberto <mrgomez.dev@gmail.com>
2025-04-20 23:26:21 -0600

    Filtrar resultados de busqueda

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -6,15 +6,29 @@ import { HousingLocation } from './housing-location';
   templateUrl: './housing-list.component.html',
   styleUrls: ['./housing-list.component.css']
 })
+
 export class HousingListComponent implements OnInit {
   @Input() locationList: HousingLocation[] = [];
+  results: HousingLocation[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.results = this.locationList;
   }
 
   searchHousingLocations(searchText: string) {
     console.log(searchText);
+    if (!searchText) {
+      this.results = this.locationList;
+      return;
+    }
+    this.results = this.locationList.filter(
+      (location: HousingLocation) => location.city
+      .toLowerCase()
+      .includes(
+        searchText.toLowerCase()
+      )
+    );
   }
 }
